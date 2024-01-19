b39b03632329aa392cef099afedac796943b184a
Narayan <narayandhungel156@gmail.com>
2024-01-19 09:35:23 +0545

    day-2

--- index.js
+++ index.js
@@ -1,12 +1,111 @@
-var age = 20;
-// can't redeclare block scope ; 
-var names = [];
-// names.push(1)
-// Argument of type 'number' is not assignable to parameter of type 'string'.ts(2345)
+// annotation
+var number = 20;
+var employee = [];
 // function 
-function sume(a, b) {
+function sub(a, b) {
+    return a - b;
+}
+// type annotation of function 
+function add(a, b) {
     return a + b;
 }
-function sum(a, b) {
-    return a + b;
+//Tuples 
+var address;
+address = [11, 'ktm', 11111];
+//  object
+var person;
+// ? is used for optional properties 
+person = { name: 'john', age: 22 };
+person = { name: 'narayan' };
+// function declaration using optional parameter 
+function addition(a, b) {
+    return b ? a + b : a;
 }
+var p;
+p = { name: 'john', id: 30, age: 30 };
+// union 
+var p1;
+p1 = { name: 'john', age: 23, id: 4, roll: 76 };
+p1 = { name: 'john', age: 23, roll: 4, };
+// intersection
+var p2;
+p2 = { name: 'doe', age: 4, id: 4, roll: 44 };
+var c;
+c = 1;
+c = "one";
+var n = []; //never array 
+// n.push('hi')
+// class in javascript 
+var Car = /** @class */ (function () {
+    function Car(brand) {
+        this.brand = brand;
+    }
+    Car.prototype.getBrand = function () {
+        console.log(this.brand);
+    };
+    return Car;
+}());
+var c1 = new Car('tesla');
+c1.getBrand();
+// class in typescript 
+// about public  key word 
+var CarX = /** @class */ (function () {
+    function CarX(brand) {
+        this.brand = brand;
+    }
+    CarX.prototype.getBrand = function () {
+        console.log(this.brand);
+    };
+    return CarX;
+}());
+var c2 = new CarX('BMW');
+c2.getBrand();
+console.log(c2.brand);
+// about private   key word 
+var CarY = /** @class */ (function () {
+    function CarY(brand) {
+        this.brand = brand;
+    }
+    CarY.prototype.getBrand = function () {
+        console.log(this.brand);
+    };
+    return CarY;
+}());
+var c3 = new CarY('Ferrari');
+c3.getBrand();
+// console.log(c3.brand); 
+// about protected keyword 
+var CarZ = /** @class */ (function () {
+    function CarZ(brand) {
+        this.brand = brand;
+    }
+    CarZ.prototype.getBrand = function () {
+        console.log(this.brand);
+    };
+    return CarZ;
+}());
+var c4 = new CarZ('toyota');
+c4.getBrand();
+var CarC = /** @class */ (function () {
+    function CarC(brand, color) {
+        this.brand = brand;
+        this.color = color;
+    }
+    return CarC;
+}());
+var CarA = /** @class */ (function () {
+    function CarA(brand, color, release) {
+        this.brand = brand;
+        this.color = color;
+        this.release = release;
+    }
+    return CarA;
+}());
+// generics 
+function gen(a, b) {
+    return [a, b];
+}
+gen('1', '2');
+gen(1, 2);
+console.log(gen('1', '2'));
+console.log(gen(1, 2));
--- index.ts
+++ index.ts
@@ -1,27 +1,207 @@
-let age:number = 20 ; 
+// annotation
+let number:number = 20 ; 
 
-// can't redeclare block scope ; 
 
-const names:string[] = [];
-// names.push(1)
- // Argument of type 'number' is not assignable to parameter of type 'string'.ts(2345)
+const employee:string[] = [];
 
 
  // function 
- function sume(a,b) {
-    return a+b;
+ function sub(a,b) {
+    return a-b;
  }
 
- function sum(a:number, b:number) :number {
+
+ // type annotation of function 
+ function add(a:number, b:number) :number {
     return a+b;
  }
 
 //Tuples 
  let address :[number, string , number];
 
- address = [11, 'budhabare', 11111];
+ address = [11, 'ktm', 11111];
 
 //  object
 let person :{name:string, age?:number};
 
-person = {name :'narayan', age}
+// ? is used for optional properties 
+
+person = {name :'john', age:22};
+person = {name:'narayan'};
+
+// function declaration using optional parameter 
+
+function addition (a:string , b?:string):string{
+    return b? a+b :a;
+}
+
+// function addition (a?:string , b:string):string{
+    // return b? a+b :a;
+// } // this is an invalid syntax because optional parameter is always after the compulsory parameter
+
+
+
+// interface
+
+interface person {
+    name :string ;
+    id :number;
+    age :number ;
+}
+
+
+interface student {
+    name :string ;
+    roll :number;
+    age :number ;
+}
+
+let p: person ;
+
+p= {name :'john' , id: 30, age : 30 };
+
+
+// union 
+let p1 : person | student 
+
+p1 = {name : 'john' , age:23, id:4, roll:76 }
+p1= {name : 'john' , age:23, roll:4, }
+
+
+
+// intersection
+let p2 : person & student   
+p2 = {name : 'doe' , age:4, id:4, roll :44 }
+
+
+// type alias 
+
+type Count = string | number; 
+
+let c:Count; 
+c = 1;
+c="one";
+
+type X = string & number // never ;
+
+const n:[] =[] //never array 
+
+// n.push('hi')
+
+// class in javascript 
+class Car {
+    brand; 
+    constructor (brand) {
+        this.brand = brand ;
+    }
+
+    getBrand() {
+        console.log(this.brand)
+    }
+}
+
+let c1 = new Car ('tesla');
+c1.getBrand();
+
+
+// class in typescript 
+// about public  key word 
+class CarX {
+    
+    constructor (public brand) {
+    }
+
+    getBrand() {
+        console.log(this.brand)
+    }
+}
+
+let c2 = new CarX ('BMW');
+c2.getBrand();
+console.log(c2.brand);
+
+
+// about private   key word 
+class CarY {
+    
+    constructor (private brand) {
+    }
+
+    getBrand() {
+        console.log(this.brand)
+    }
+}
+
+let c3 = new CarY ('Ferrari');
+c3.getBrand();
+// console.log(c3.brand); 
+
+// about protected keyword 
+class CarZ {
+    
+    constructor (protected brand) {
+    }
+
+    getBrand() {
+        console.log(this.brand)
+    }
+}
+
+let c4 = new CarZ ('toyota');
+c4.getBrand();
+// console.log(c4.brand); 
+
+
+// implements
+
+interface ICar {
+    brand :string; 
+    color :string;
+}
+
+
+interface ICar2 {
+   release: number; 
+}
+
+class CarC implements ICar {
+    constructor(public brand, public color) {
+
+    }
+}
+
+
+class CarA implements ICar, ICar2 {
+    constructor(public brand, public color , public release) {
+
+    }
+}
+
+
+// generics 
+//generic allows us to define type as a variables
+function gen<T> (a:T, b:T) :T[]{
+    return [a,b]
+}
+
+gen<string> ('1','2');
+gen<number> (1,2);
+
+console.log(gen<string> ('1','2'));
+console.log(gen<number> (1,2));
+
+// in case of array 
+// array itself is a generics 
+
+gen<Array<number>>([1],[2]);
+console.log(gen<Array<number>>([1],[2]));
+
+// use of extend 
+function addUser <T extends {id:string} >(user:T){
+    return user.id; 
+}
+
+// any type 
+function noRestriction(a :any ,b:any) :any  { 
+    return a+b;
+}
